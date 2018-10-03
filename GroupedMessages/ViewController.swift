b05e64683d831257e8c25d9b6aecc658f06cdaa2
liroy y <liroy.yarimi@gmail.com>
2018-10-03 11:18:57 +0300

    Create a TableView

--- GroupedMessages/ViewController.swift
+++ GroupedMessages/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    fileprivate let cellId = "id"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.prefersLargeTitles = true //make the title large
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = "We want to provide a longer string that is actually going to wrap onto the next line and maybe even a third line"
+
+        cell.textLabel?.numberOfLines = 0//make the cell size dynamic
+        
+        return cell
     }
 
 
